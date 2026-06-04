cab164c4e5c554a9599f39f2a4e3ea32e5655dee
AKKI <ankitkumarpadhan3333@gmail.com>
2026-06-04 16:49:27 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,102 @@
-
+const questions = [
+{
+question:"Which pigment is primarily responsible for photosynthesis?",
+options:["Chlorophyll a","Carotene","Xanthophyll","Phycoerythrin"],
+answer:0
+},
+{
+question:"The powerhouse of plant cells is:",
+options:["Nucleus","Golgi body","Mitochondria","Ribosome"],
+answer:2
+},
+{
+question:"C3 cycle is also known as:",
+options:["Calvin Cycle","Krebs Cycle","Glycolysis","Hatch-Slack Cycle"],
+answer:0
+},
+{
+question:"Water potential is represented by:",
+options:["Ψw","Ψs","Ψp","Ψm"],
+answer:0
+},
+{
+question:"Which tissue conducts water in plants?",
+options:["Phloem","Cambium","Xylem","Cortex"],
+answer:2
+},
+{
+question:"The study of algae is called:",
+options:["Mycology","Phycology","Bryology","Pteridology"],
+answer:1
+},
+{
+question:"Double fertilization is characteristic of:",
+options:["Gymnosperms","Angiosperms","Pteridophytes","Bryophytes"],
+answer:1
+},
+{
+question:"Plant growth hormone promoting cell elongation:",
+options:["Ethylene","ABA","Auxin","Cytokinin"],
+answer:2
+},
+{
+question:"Photorespiration occurs in:",
+options:["C3 plants","C4 plants","CAM plants","All equally"],
+answer:0
+},
+{
+question:"Functional unit of heredity:",
+options:["Chromosome","Gene","DNA","RNA"],
+answer:1
+},
+{
+question:"Mendel worked on:",
+options:["Maize","Pea","Rice","Wheat"],
+answer:1
+},
+{
+question:"Nitrogen fixation in legumes is done by:",
+options:["Azotobacter","Rhizobium","Nostoc","Anabaena"],
+answer:1
+},
+{
+question:"Largest group of plants:",
+options:["Bryophytes","Gymnosperms","Angiosperms","Pteridophytes"],
+answer:2
+},
+{
+question:"Cell wall mainly contains:",
+options:["Cellulose","Protein","Lipid","Starch"],
+answer:0
+},
+{
+question:"ATP is synthesized in:",
+options:["Chloroplast","Mitochondria","Both","None"],
+answer:2
+},
+{
+question:"The process of water loss from leaves:",
+options:["Respiration","Transpiration","Guttation","Diffusion"],
+answer:1
+},
+{
+question:"DNA replication occurs during:",
+options:["G1","S phase","G2","M phase"],
+answer:1
+},
+{
+question:"Scientific name of mango:",
+options:["Mangifera indica","Azadirachta indica","Oryza sativa","Triticum aestivum"],
+answer:0
+},
+{
+question:"Ecology term coined by:",
+options:["Darwin","Haeckel","Linnaeus","Hooker"],
+answer:1
+},
+{
+question:"Red Data Book is related to:",
+options:["Medicinal plants","Endangered species","Agriculture","Forestry"],
+answer:1
+}
+];
